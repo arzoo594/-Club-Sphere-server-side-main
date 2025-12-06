1716f2aec9a80718f3463049341eb00c187ca212
arzoo594 <arzooahmed0170609@gmail.com>
2025-12-07 00:42:54 +0600

    mongodb connected and user post api create in my project

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.9a0hyyx.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    const db = client.db("ClubsSphere");
+    const membersCollection = db.collection("members");
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+
+      const result = await membersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("Hello Worlds!");
 });
